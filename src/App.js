a0558a56b7c533c1e1ccd063cf9a188a804829e1
yxcho <choyangxuan@me.com>
2020-01-21 00:59:25 +1100

    first

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Frankenstein"
+        },
+        { name: "Dracula" },
+        { name: "Zombie" }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+        {
+  this.state.monsters.map(monster => <h1>{monster.name}</h1>)
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
