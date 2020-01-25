8c007ad8f809f12eb91ab2e5b0150ee4ef386fb8
yxcho <choyangxuan@me.com>
2020-01-26 01:36:33 +0800

    Updates

---
+++ precache-manifest.9c89cce03083ce167aaedb5f57bd7017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f979ac1ee7e1dbdc28ce88282595061a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "25f43e33688851313e1e",
+    "url": "/monsters-rolodex/static/css/main.a34828b6.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25f43e33688851313e1e",
+    "url": "/monsters-rolodex/static/js/main.eb2777c1.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
